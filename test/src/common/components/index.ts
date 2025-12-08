eb0c63ec54daba9f12b847910a0195aecbdb568a
RussuIana <afanasiana@mail.ru>
2025-12-08 14:12:27 +0200

    searchInput

--- test/src/common/components/index.ts
+++ test/src/common/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./header/Header"
 export { Footer } from "./footer/Footer"
 export { NavButton } from "./navButton/NavButton.tsx"
+export { SearchInput } from "./searchInput/SearchInput.tsx"
--- test/src/common/pages/Main.tsx
+++ test/src/common/pages/Main.tsx
@@ -1,11 +1,15 @@
 import {MovieCategories} from "@/features/discoverMovies/ui/movieCategories/MovieCategories.tsx";
+import {SearchInput} from "@/common/components";
+
 
 export const Main = () => {
     return (
         <div>
-            <h1>Welcome <br/>
-                Browse highlighted titles from TMDB</h1>
-            <input type="text"/> <button>search</button>
+            <div>
+                <h1>Welcome </h1>
+                <h2>Browse highlighted titles from TMDB</h2>
+                <SearchInput/>
+            </div>
             <MovieCategories/>
         </div>
     )
