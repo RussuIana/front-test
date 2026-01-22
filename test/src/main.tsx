ccb4c6ab316428a5280044ae94ea049cdff380d2
RussuIana <afanasiana@mail.ru>
2026-01-22 11:05:36 +0200

    Fix Vite base for Vercel

--- test/src/main.tsx
+++ test/src/main.tsx
@@ -1,16 +1,16 @@
 import {createRoot} from "react-dom/client"
 import "./index.css"
 
-// import {BrowserRouter} from "react-router"
+import {BrowserRouter} from "react-router"
 import {Provider} from "react-redux"
 import {store} from "@/app/store.ts"
 import {App} from "@/app/App.tsx";
-import {HashRouter} from "react-router-dom";
+
 
 createRoot(document.getElementById("root")!).render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </HashRouter>,
+    </BrowserRouter>,
 )
