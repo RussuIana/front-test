f0c894a259185fe43701e948de0ee73efd157a98
RussuIana <afanasiana@mail.ru>
2026-01-22 10:06:19 +0200

    fix vercel build: update config and deps

--- test/src/main.tsx
+++ test/src/main.tsx
@@ -1,15 +1,16 @@
 import {createRoot} from "react-dom/client"
 import "./index.css"
 
-import {BrowserRouter} from "react-router"
+// import {BrowserRouter} from "react-router"
 import {Provider} from "react-redux"
 import {store} from "@/app/store.ts"
 import {App} from "@/app/App.tsx";
+import {HashRouter} from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
 )
--- test/vite.config.ts
+++ test/vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 export default defineConfig({
-  plugins: [
+    base:  "/front-test/", // ✅ ВАЖНО: имя репозитория
+    plugins: [
     react({
       babel: {
         plugins: ["babel-plugin-styled-components"],
